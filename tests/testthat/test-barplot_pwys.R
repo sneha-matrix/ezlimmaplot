25c92c6dc4149d0444088234a47ef1255e2d831f
sneha-matrix <bioinfovinay7@gmail.com>
2019-11-15 02:12:55 +0530

    uploading barplot of significance per pathway test

--- tests/testthat/test-barplot_pwys.R
+++ tests/testthat/test-barplot_pwys.R
@@ -1,2 +1,23 @@
 context("barplot of significance per pathway")
 
+test_that("direction properly specified", {
+  expect_error(barplot_pwys(rc))
+  expect_error(barplot_pwys(rc, prefix.v = "First3"))
+  expect_error(barplot_pwys(tab=rc, direction = "Up"))
+
+  expect_silent(barplot_pwys(tab=rc, prefix.v = "First3", direction = "Up"))
+  expect_silent(barplot_pwys(tab=rc, prefix.v = "Last3", direction = "Up"))
+  expect_message(barplot_pwys(tab=rc, prefix.v = "Last3vsFirst3", direction = "Down"))
+})
+
+test_that("barplot object non-vdiffr", {
+  bar_plot<-barplot_pwys(tab=rc, prefix.v = "First3", direction = "Up")
+  expect_equal(bar_plot$data["pwy1","Direction"] , rc["pwy1","First3.Direction"])	
+  expect_equal(bar_plot$data["pwy1","neglog10p"] , log10(rc["pwy1" ,"First3.p"])*-1)	
+})
+
+test_that("red color vdiffr", {
+  #verified that bar is of red color 
+  redbar <- function() barplot_pwys(tab=rc, prefix.v = "First3", direction = "Up")
+  vdiffr::expect_doppelganger(title="red bar", fig=redbar)
+})
